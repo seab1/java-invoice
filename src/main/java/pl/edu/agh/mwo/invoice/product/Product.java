791d2f3120ecaa09ac745be1611c86bb83f526ea
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-01-19 09:53:41 +0100

    Fixed first test of ProtuctTest, Product getters updated

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -2,32 +2,36 @@ package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
 
-public abstract class Product {
+public abstract class Product
+{
 	private final String name;
-
 	private final BigDecimal price;
-
 	private final BigDecimal taxPercent;
 
-	protected Product(String name, BigDecimal price, BigDecimal tax) {
+	protected Product(String name, BigDecimal price, BigDecimal tax)
+	{
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
 	}
 
-	public String getName() {
-		return null;
+	public String getName()
+	{
+		return this.name;
 	}
 
-	public BigDecimal getPrice() {
-		return null;
+	public BigDecimal getPrice()
+	{
+		return this.price;
 	}
 
-	public BigDecimal getTaxPercent() {
-		return null;
+	public BigDecimal getTaxPercent()
+	{
+		return this.taxPercent;
 	}
 
-	public BigDecimal getPriceWithTax() {
+	public BigDecimal getPriceWithTax()
+	{
 		return null;
 	}
 }
--- src/test/java/pl/edu/agh/mwo/invoice/product/ProductTest.java
+++ src/test/java/pl/edu/agh/mwo/invoice/product/ProductTest.java
@@ -8,50 +8,59 @@ import org.junit.Test;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
-public class ProductTest {
+public class ProductTest
+{
 	@Test
-	public void testProductNameIsCorrect() {
+	public void testProductNameIsCorrect()
+	{
 		Product product = new OtherProduct("buty", new BigDecimal("100.0"));
 		Assert.assertEquals("buty", product.getName());
 	}
 
 	@Test
-	public void testProductPriceAndTaxWithDefaultTax() {
+	public void testProductPriceAndTaxWithDefaultTax()
+	{
 		Product product = new OtherProduct("Ogorki", new BigDecimal("100.0"));
 		Assert.assertThat(new BigDecimal("100"), Matchers.comparesEqualTo(product.getPrice()));
 		Assert.assertThat(new BigDecimal("0.23"), Matchers.comparesEqualTo(product.getTaxPercent()));
 	}
 
 	@Test
-	public void testProductPriceAndTaxWithDairyProduct() {
+	public void testProductPriceAndTaxWithDairyProduct()
+	{
 		Product product = new DairyProduct("Szarlotka", new BigDecimal("100.0"));
 		Assert.assertThat(new BigDecimal("100"), Matchers.comparesEqualTo(product.getPrice()));
 		Assert.assertThat(new BigDecimal("0.08"), Matchers.comparesEqualTo(product.getTaxPercent()));
 	}
 
 	@Test
-	public void testPriceWithTax() {
+	public void testPriceWithTax()
+	{
 		Product product = new DairyProduct("Oscypek", new BigDecimal("100.0"));
 		Assert.assertThat(new BigDecimal("108"), Matchers.comparesEqualTo(product.getPriceWithTax()));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProductWithNullName() {
+	public void testProductWithNullName()
+	{
 		new OtherProduct(null, new BigDecimal("100.0"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProductWithEmptyName() {
+	public void testProductWithEmptyName()
+	{
 		new TaxFreeProduct("", new BigDecimal("100.0"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProductWithNullPrice() {
+	public void testProductWithNullPrice()
+	{
 		new DairyProduct("Banany", null);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProductWithNegativePrice() {
+	public void testProductWithNegativePrice()
+	{
 		new TaxFreeProduct("Mandarynki", new BigDecimal("-1.00"));
 	}
 }
